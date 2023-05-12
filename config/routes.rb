44570e66a5ea1cf3e8de972f8fc5b58b71cf63d4
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2023-05-12 11:23:23 +0900

    added platform to bundle lock

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: "lists#index"
+  # A user can see all the list
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create]
+  end
 end
